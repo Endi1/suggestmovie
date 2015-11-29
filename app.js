b2b126cabca746c1be6b3cc8665e376b1be03d15
Endi <endisukaj@gmail.com>
2015-11-29 23:14:21 +0100

    Added navbar and about page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var IndexRoute = require('react-router').IndexRoute;
 var App = require('./components/App');
 var GenreDisplay = require('./components/GenreDisplay');
 var MovieDisplay = require('./components/MovieDisplay');
+var About = require('./components/AboutDisplay');
 
 var createHistory = require('history').createHashHistory;
 
@@ -19,6 +20,7 @@ ReactDOM.render(
     <Route path="/" component={App} >
       <IndexRoute component={GenreDisplay} />
       <Route path="1" component={MovieDisplay} />
+      <Route path="about" component={About} />
     </Route>
   </Router>,
   document.getElementById('app')
